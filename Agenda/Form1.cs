71213622f57c53b8ff0bbec27f91cef12ab71799
Ezequiel <ezequielsennesantos@gmail.com>
2022-09-14 08:50:10 -0300

    bb  k h jkk vb

--- Agenda/Form1.cs
+++ Agenda/Form1.cs
@@ -64,5 +64,16 @@ namespace Agenda
 
 
         }
+
+        private void dgwTabela_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgwTabela.CurrentRow.Index != -1)
+            {
+                txtID.Text = dgwTabela.CurrentRow.Cells[0].Value.ToString();
+                txtNome.Text = dgwTabela.CurrentRow.Cells[1].Value.ToString();
+                txtEmail.Text = dgwTabela.CurrentRow.Cells[2].Value.ToString();
+                //btnInserir.Text = "NOVO";
+            }
+        }
     }
 }
